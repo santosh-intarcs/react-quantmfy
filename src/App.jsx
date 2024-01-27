e74e3071a3ba629991ec686e74ec079d3896522f
madhumathi0213 <madhumathi@pentafox.in>
2024-01-27 16:13:20 +0530

    added post css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './App.css'
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider } from '@mantine/core';
 import {
   RouterProvider,
 } from "react-router-dom";
 import { BroswerRoute } from './components/Navigation/Router';
 
+const theme = createTheme({
+  fontFamily: 'Inter',
+  white: 'rgba(255, 255, 255, 1)',
+  breakpoints: {
+    xs: '30em',
+    sm: '48em',
+    md: '64em',
+    lg: '74em',
+    xl: '90em',
+  },
+});
+
 function App() {
 
   return (
-    <MantineProvider theme={{
-      fontFamily: 'Inter',
-    }}>
+    <MantineProvider theme={theme}>
       <React.StrictMode>
         <RouterProvider router={BroswerRoute} />
       </React.StrictMode>
